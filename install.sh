7cff2053babae264ea9cb502036ada49bad8b7ea
Cody Poll <cjpoll@gmail.com>
2013-12-24 08:36:06 +0000

    Added a default main file for C++ programs

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ echo "Copying lib files to /usr/local/lib/cpp/"
 cp ./cpp/Makefile /usr/local/lib/program_setup/cpp/
 cp ./cpp/.ycm_extra_conf.py /usr/local/lib/program_setup/cpp/
 cp ./cpp/_tests.cpp /usr/local/lib/program_setup/cpp/
+cp ./cpp/main.cpp /usr/local/lib/program_setup/cpp/
 echo "Copied lib files to /usr/local/lib/cpp/"
 
 echo "Copying lib files to /usr/local/lib/program_setup/ruby"
