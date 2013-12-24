9d73ec5b3a579122c280b7b07a6718f68766ec40
Cody Poll <cjpoll@gmail.com>
2013-12-24 07:23:08 +0000

    Added C++ support

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ echo "Needs to be run as root to work"
 
 echo "Creating lib directories in /usr/local/lib/"
 mkdir -p /usr/local/lib/program_setup/c/
+mkdir -p /usr/local/lib/program_setup/cpp/
 mkdir -p /usr/local/lib/program_setup/java/
 mkdir -p /usr/local/lib/program_setup/ruby/
 mkdir -p /usr/local/lib/program_setup/erlang/
@@ -13,6 +14,11 @@ cp ./c/.ycm_extra_conf.py /usr/local/lib/program_setup/c/
 cp ./c/_test.c /usr/local/lib/program_setup/c/
 echo "Copied lib files to /usr/local/lib/c/"
 
+echo "Copying lib files to /usr/local/lib/cpp/"
+cp ./cpp/Makefile /usr/local/lib/program_setup/cpp/
+cp ./cpp/.ycm_extra_conf.py /usr/local/lib/program_setup/cpp/
+echo "Copied lib files to /usr/local/lib/cpp/"
+
 echo "Copying lib files to /usr/local/lib/program_setup/ruby"
 cp ./ruby/Rakefile /usr/local/lib/program_setup/ruby/
 cp ./ruby/_spec.rb /usr/local/lib/program_setup/ruby/
@@ -23,12 +29,13 @@ cp ./erlang/Makefile /usr/local/lib/program_setup/erlang/
 cp ./erlang/rebar.config /usr/local/lib/program_setup/erlang/
 echo "Copied lib files to /usr/local/lib/program_setup/erlang"
 
-echo "Copying lib files to /usr/local/lib/program_setup/erlang"
+echo "Copying lib files to /usr/local/lib/program_setup/java"
 cp ./java/.syntastic_javac_config /usr/local/lib/program_setup/java/
 cp ./java/Makefile /usr/local/lib/program_setup/java/
 
 echo "Copying executable files to /usr/local/bin"
 cp ./c/cprog_setup /usr/local/bin/
+cp ./cpp/cpp_setup /usr/local/bin
 cp ./ruby/rprog_setup /usr/local/bin/
 cp ./erlang/erlprog_setup /usr/local/bin/
 cp ./java/jprog_setup /usr/local/bin/
