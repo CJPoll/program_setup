92ea8dced48e6da02268cf2e956d8de74d85229a
Cody Poll <cjpoll@gmail.com>
2014-01-03 02:07:43 +0000

    Updated default spec for ruby projects

--- ruby/_spec.rb
+++ ruby/_spec.rb
@@ -1,30 +1,12 @@
-require('./stack.rb')
-describe "Stack" do
+require('./class.rb')
+describe "Class" do
 	before :each do
-		@stack = Stack.new
+		@class = Class.new
 	end
 
-	it "should create a stack and properly initialize it" do
-		@stack.nil?.should == false
-		@stack.size.should == 0
-		@stack.instance_variable_get(:@list).nil?.should == false
-	end
-
-	it "should push a value on the top of the stack and return the stack" do
-		stack = @stack.push 5
-		stack.should == @stack
-		@stack.size.should == 1
-	end
-
-	it "should show the top value without removing it on peek" do
-		@stack.push 5
-		@stack.peek.should == 5
-		@stack.size.should == 1
-	end
-
-	it "should remove & show the top value on pop" do
-		@stack.push 5
-		@stack.pop.should == 5
-		@stack.size.should == 0
+	it "should create an instance of class and properly initialize it" do
+		@class.nil?.should == false
+		@class.size.should == 0
+		@class.instance_variable_get(:@list).nil?.should == false
 	end
 end
